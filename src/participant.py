890e5f8eb3c988a6d7e769babb81a90e459a4f21
Jolinda Smith <jolinda@uoregon.edu>
2021-12-16 17:04:11 -0800

    took out enter key thing that does not work

--- src/participant.py
+++ src/participant.py
@@ -1,6 +1,7 @@
 # this is pycap, not the redcap class originally written for this project
 import redcap
 
+
 class Subject:
     def __init__(self, subject_id, redcap_token):
         project = redcap.Project(url='https://redcap.uoregon.edu/api/',
@@ -13,4 +14,3 @@ class Subject:
         self.redcap = data.loc[subject_id].rename(index=dict(session_0_arm_1='s0',
                                                              session_1_arm_1='s1',
                                                              session_2_arm_1='s2')).transpose()
-
