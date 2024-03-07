e487fb4c8248c8cdd1a4c9d7749b889a07e8fa75
jolinda <jolidna@uoregon.edu>
2024-03-06 16:42:14 -0800

    temp disabled ssl verify for redcap

--- src/participant.py
+++ src/participant.py
@@ -4,7 +4,7 @@ import redcap
 class RedcapParticipant:
     def __init__(self, subject_id, redcap_token):
         project = redcap.Project(url='https://redcap.uoregon.edu/api/',
-                                 token=redcap_token)
+                                 token=redcap_token, verify_ssl=False)
         data = project.export_records(events=['session_0_arm_1',
                                               'session_1_arm_1'],
                                       format_type='df')
